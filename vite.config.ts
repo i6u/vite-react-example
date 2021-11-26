6d9fd924ef21184928cfdf67b945e92ff91d8540
witt <mymeek@163.com>
2021-11-26 08:34:31 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {viteSingleFile} from "vite-plugin-singlefile"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react(), viteSingleFile()],
+    build: {
+        target: "esnext",
+        assetsInlineLimit: 100000000,
+        chunkSizeWarningLimit: 100000000,
+        cssCodeSplit: false,
+        brotliSize: false,
+        rollupOptions: {
+            inlineDynamicImports: true,
+            output: {
+                manualChunks: () => "everything.js",
+            },
+        },
+    },
 })
